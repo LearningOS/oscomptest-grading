4ab63c11a343cf695a165daa635f8e2d4d720b82
wingrew <114449728+wingrew@users.noreply.github.com>
2025-03-27 00:54:44 +0800

    Update index.ts

--- spider/src/config/index.ts
+++ spider/src/config/index.ts
@@ -5,7 +5,7 @@ export const config = dotenv.config();
 export const organiztion: string = 'LearningOS';            // 组织
 export const fullOrganization: string = '19380377-learningos-classroom-oscomp'; // classroom名称
 export const assignment = 'oscomp-test';   // assignment
-export const works = ['main']; // assignment 的不同情况
+export const works = ['basic_glibc', 'libc_glibc', 'lua_glibc', 'busybox_glibc', 'iozone_glibc', 'basic_musl', 'libc_musl', 'lua_musl', 'busybox_musl', 'iozone_musl']; // assignment 的不同情况
 export const AUTH_TOKEN = process.env['TOKEN'];
 export const SESSION_TOKEN = process.env['SESSION_TOKEN'];
 
